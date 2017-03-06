1aa391a220c27592347c75836d97ff86187dca92
nitss007 <nitss007@gmail.com>
2017-03-06 14:50:51 +0530

    Add files via upload

---
+++ helloworld/src/main/java/helloworld/HelloWorldService.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class HelloWorldService {
+		 
+		private String name;
+	 
+		public void setName(String name) {
+			this.name = name;
+		}
+	 
+		public String sayHello() {
+			return "Hello! " + name;
+		}
+}
